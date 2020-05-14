774313c18a6d63b13d1826353e2eef15bc9aed17
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-14 13:53:15 +0100

    added and setup project apps

--- notebook/settings/base.py
+++ notebook/settings/base.py
@@ -25,6 +25,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'users',
+    'notes',
+    'search',
 ]
 
 MIDDLEWARE = [
