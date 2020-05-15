5802b9523326706f92bf93896a45dc1356a99100
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-15 01:51:48 +0100

    search history implemented

--- search/models.py
+++ search/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class SearchHistory(models.Model):
+    """
+    Model to store a search query made by a User.
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='search_history'
+    )
+    query = models.CharField(max_length=255)
+    search_date = models.DateTimeField(auto_now_add=True)
--- search/views.py
+++ search/views.py
@@ -1,42 +1,54 @@
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from django.views.generic import ListView
 
-from notes.models import Note
 from .utils import search_django_site
+from .models import SearchHistory
+from notes.models import Note
 
-
+#TODO: anonymous users
 class Search(ListView):
     """
     Search and return a list Note objects. Search query queries against Note title.
     """
     template_name = 'search.html'
 
-    def _get_search_query(self):
-        """
-        Get and return the search query in the url name `q`.
-        """
-        return self.request.GET.get('q')
-
     def get_queryset(self):
         """
         Get the User's Notes ordered by ranking with given search query. Implements a full-text
         search on the fields: `title` and `content`
         """
-        search_query = self._get_search_query()
+        search_query = self.get_search_query()
         if search_query is not None:
-            search_django_site(search_query)
             user = self.request.user
+            # Log search query.
+            SearchHistory.objects.create(user=user, query=search_query)
+            # Search for matching Notes.
+            search_django_site(search_query)
             vector = SearchVector('title', 'content')
             query = SearchQuery(search_query)
-            return Note.objects.filter(user=user).annotate(rank=SearchRank(vector, query)).order_by('-rank')[:10]
+            results = Note.objects.filter(user=user).annotate(rank=SearchRank(vector, query)).order_by('-rank')[:10]
+            return results
         return
+
+    def get_search_query(self):
+        """
+        Get and return the search query in the url name `q`.
+        """
+        return self.request.GET.get('q')
+
+    def get_search_history(self):
+        """
+        Return the 5 most recent searches the User has made.
+        """
+        history = self.request.user.search_history.order_by('-search_date').values('query')[:5]
+        print(history)
+        return history
 
     def get_django_search_results(self):
         """
         Return the results from Django documentation.
         """
-        search_query = self._get_search_query()
-        print(search_query)
+        search_query = self.get_search_query()
         if search_query is not None:
             return search_django_site(search_query)
         return 
