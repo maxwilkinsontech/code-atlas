3a89fc0a010ff06021d8af57d3a81b3936db6a90
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-15 02:31:59 +0100

    changed settings file names

--- notebook/settings/local.py
+++ notebook/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 from decouple import Csv, Config, RepositoryEnv
 
 # Set config as .env.dev file
-env_path = 'notebook/settings/.env.stage'
+env_path = 'notebook/settings/.env.local'
 env_config = Config(RepositoryEnv(env_path))
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -27,3 +27,24 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Email Settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+
+# Python Social Auth
+
+SOCIAL_AUTH_GITHUB_KEY = env_config.get('SOCIAL_AUTH_GITHUB_KEY')
+SOCIAL_AUTH_GITHUB_SECRET = env_config.get('SOCIAL_AUTH_GITHUB_SECRET')
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env_config.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env_config.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
