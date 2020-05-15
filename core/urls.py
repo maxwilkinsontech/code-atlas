5e4364d740e4895bf7dc070966681d9fc66f0e41
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-15 20:31:29 +0100

    privacy policy

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView
 from django.urls import path
 
 from . import views
@@ -5,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('privacy-policy/', TemplateView.as_view(template_name='privacy_policy.html'), name='privacy_policy'),
+    path('term-of-service/', TemplateView.as_view(template_name='terms_of_service.html'), name='terms_of_service'),
 ]
