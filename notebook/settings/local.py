d63af3609eb318c3386fe13887ecd0a36032b92e
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-16 20:07:17 +0100

    static folder added to base

--- notebook/settings/local.py
+++ notebook/settings/local.py
@@ -24,13 +24,6 @@ DATABASES = {
     }
 }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 
 # Email Settings
 
