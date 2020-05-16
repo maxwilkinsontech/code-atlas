14f0dcb826085955f1541848849aae8e523ff3f9
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-16 01:53:24 +0100

    added tests to privacy views

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,21 @@
 from django.views.generic import TemplateView
 from django.urls import path
 
-from . import views
-
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('privacy-policy/', TemplateView.as_view(template_name='privacy_policy.html'), name='privacy_policy'),
-    path('term-of-service/', TemplateView.as_view(template_name='terms_of_service.html'), name='terms_of_service'),
+    path(
+        '', 
+        TemplateView.as_view(template_name='index.html'), 
+        name='home'
+    ),
+    path(
+        'privacy-policy/', 
+        TemplateView.as_view(template_name='privacy_policy.html'), 
+        name='privacy_policy'
+    ),
+    path(
+        'terms-of-service/', 
+        TemplateView.as_view(template_name='terms_of_service.html'), 
+        name='terms_of_service'
+    ),
 ]
