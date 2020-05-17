c1c45f11bcc3ef078163eb2659ce70b22d38a782
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-17 20:22:08 +0100

    changed admin url

--- codeatlas/urls.py
+++ codeatlas/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('code-atlas/admin/', admin.site.urls),
     path('', include('social_django.urls', namespace='social')),
     path('', include('core.urls')),
     path('notes/', include('notes.urls')),
