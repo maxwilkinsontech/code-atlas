9e94cf3584fd882ef0f4838914ba2e69d8e13626
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-20 12:47:39 +0100

    toggle notes mode buttons

--- notes/urls.py
+++ notes/urls.py
@@ -5,8 +5,8 @@ from . import views, api
 
 urlpatterns = [
     path('', views.NotesView.as_view(), name='notes'),
-    path('mode/edit/', views.NotesEditView.as_view(), name='notes_quick_edit'),
-    path('mode/tags/', views.NotesTagsView.as_view(), name='notes_tags'),
+    path('mode/edit/', views.NotesEditView.as_view(), name='notes_mode_edit'),
+    path('mode/tags/', views.NotesTagsView.as_view(), name='notes_mode_tags'),
     path('create/', views.CreateNoteView.as_view(), name='create_note'),
     path('create/clone/<pk>/', views.CloneNoteView.as_view(), name='clone_note'),
     path('view/<pk>/', views.ViewNoteView.as_view(), name='view_note'),
