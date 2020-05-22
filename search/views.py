d8418675a214c7779a240e25e1fc47c367c7393a
Max Wilkinson <maxwilkinson99@gmail.com>
2020-05-22 14:44:38 +0100

    search moved to single view

--- search/views.py
+++ search/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
-from django.urls import reverse_lazy
 
 from .utils import SearchUtil
 
@@ -10,9 +9,9 @@ class SearchView(LoginRequiredMixin, ListView):
     Search and return a list Note objects. Search query queries against Note title.
     """
     template_name = 'search.html'
-    paginate_by = 12
+    paginate_by = 24
 
-    def get_queryset(self):
+    def get_queryset(self, log=True):
         """
         If there is a search_query present in the url, search the given User's Notes. Make a log of
         the search query for the User.
@@ -20,7 +19,10 @@ class SearchView(LoginRequiredMixin, ListView):
         search_query = self.request.GET.get('q', '')
         if search_query:
             user = self.request.user
-            search_util = SearchUtil(search_query, user=user)
+            search_util = SearchUtil(search_query, user=user, log=log)
             results = search_util.get_search_results()
             return results
         return []
+
+    def get_public_notes(self):
+        return self.get_queryset(log=False)
